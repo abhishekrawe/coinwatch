f6eec9727a3dd6e98891724a0294fd70873f45f9
abhishekrawe <rajputabhisheksingh53@gmail.com>
2023-03-19 00:55:06 +0530

    some changes on headers

--- components/Header.js
+++ components/Header.js
@@ -3,7 +3,6 @@ import {
     CollectionIcon,
     HomeIcon,
     LightningBoltIcon,
-    SearchIcon,
     UserIcon,
 } from "@heroicons/react/outline";
 
@@ -12,7 +11,7 @@ import HeaderItem from "./HeaderItem";
 
 export default function Header() {
    return (
-    <header className="flex flex-col sm:flex-row m-5 justify-between items-center h-auto">
+    <header className="flex flex-col sm:flex-row m-5 ml-10 justify-between items-center h-auto">
      <Image 
        className="object-contain" 
        src="https://www.zapit.io/zapit_logo.svg" 
@@ -23,10 +22,9 @@ export default function Header() {
      <div className="flex flex-grow justify-evenly max-w-2xl">
         <HeaderItem title="HOME" Icon={HomeIcon} />
         <HeaderItem title="TRENDING" Icon={LightningBoltIcon} />
-        <HeaderItem title="HOME" Icon={BadgeCheckIcon} />
-        <HeaderItem title="HOME" Icon={CollectionIcon} />
-        <HeaderItem title="HOME" Icon={SearchIcon} />
-        <HeaderItem title="HOME" Icon={UserIcon} />
+        <HeaderItem title="COINS" Icon={BadgeCheckIcon} />
+        <HeaderItem title="TOKEN" Icon={CollectionIcon} />
+        <HeaderItem title="USER" Icon={UserIcon} />
      </div>
     </header>
    )
