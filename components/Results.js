fb62436e153f82e3385bf04c398b9d84392a3f2a
abhishekrawe <rajputabhisheksingh53@gmail.com>
2023-03-19 15:36:02 +0530

    💫some styling

--- components/Results.js
+++ components/Results.js
@@ -5,7 +5,9 @@ export default function Results({ results }) {
   return (
     <div className='px-4 md:px-8 lg:px-12 xl:px-16'>
       {results.map ((result) => (
-        <Block key = {result.id} result = {result}/>
+        <div key={result.id} className='hover:bg-gray-800'>
+          <Block result={result}/>
+        </div>
       ))}
     </div>
   )
